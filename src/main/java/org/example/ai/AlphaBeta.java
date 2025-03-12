340ddb7bf60033df1a657a0235682f1fbd8e01f3
Eli Boyden <aqu4eb@virginia.edu>
2025-03-12 13:14:48 -0400

    added javadocs for AlphaBeta

--- src/main/java/org/example/ai/AlphaBeta.java
+++ src/main/java/org/example/ai/AlphaBeta.java
@@ -7,13 +7,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AlphaBeta<M> implements SearchAlgorithm<M> {
+    /**
+     * Implements SearchAlgorithm interface using minimax with
+     * <a href="https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning">alpha-beta pruning</a>.
+     *
+     * @author Eli Boyden, eboyden42
+     */
 
+    /**
+     * Indicates to what depth the algorithm will search.
+     */
     private int depth;
 
+    /**
+     * Constructs an AlphaBeta object initializing the depth.
+     *
+     * @param depth The depth for this instance of Minimax
+     */
     public AlphaBeta(int depth) {
         this.depth = depth;
     }
 
+    /**
+     * Finds the best move given a game state by running alpha-beta on all possible moves from the given state.
+     *
+     * @param state the current game state
+     * @return the best move for the current player
+     */
     public M findBestMove(GameState<M> state) {
         List<M> possibleMoves = state.getPossibleMoves();
         int highestIndex = 0;
@@ -28,6 +48,17 @@ public class AlphaBeta<M> implements SearchAlgorithm<M> {
         return possibleMoves.get(highestIndex);
     }
 
+    /**
+     * Implements <a href="https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning">alpha-beta pruning</a> to evaluate the best move for the current player.
+     *
+     * @param node The current game state being evaluated.
+     * @param depth The maximum depth to explore in the game tree.
+     * @param alpha The best value that the maximizing player can guarantee so far.
+     * @param beta The best value that the minimizing player can guarantee so far.
+     * @param isMaximizingPlayer {@code true} if the current player is the maximizing player, {@code false} if the
+     * current player is the minimizing player.
+     * @return The evaluation score for the current game state.
+     */
     public int alphabeta(GameState<M> node, int depth, int alpha, int beta, boolean isMaximizingPlayer) {
         if (depth == 0 || node.isTerminal()) {
             return node.evaluate(node.getCurrentPlayer()); // If the node is terminal, return static evaluation
